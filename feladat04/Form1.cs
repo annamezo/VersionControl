555d5dd1e628c81c469e4bd3be7a41dd0211db9a
annamezo <annamezo@zohomail.eu>
2022-11-19 17:45:28 +0100

    Feladat07

--- feladat04/Form1.cs
+++ feladat04/Form1.cs
@@ -86,6 +86,23 @@ namespace feladat04
 
         }
 
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
         public Form1()
         {
             InitializeComponent();
