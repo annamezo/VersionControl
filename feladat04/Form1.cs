2dc3e1c1d09a75002324d11e9e0b56ba3db35fd1
annamezo <annamezo@zohomail.eu>
2022-11-19 17:30:49 +0100

    Feladat06

--- feladat04/Form1.cs
+++ feladat04/Form1.cs
@@ -17,9 +17,41 @@ namespace feladat04
         List<Flat> flats;
         RealEstateEntities re = new RealEstateEntities();
 
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         void LoadData()
         {
             flats = re.Flat.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Source + '\n' + ex.Message); //hiba forrása, sortörés, hibaüzenet
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlApp = null;
+                xlWB = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            
         }
 
         public Form1()
