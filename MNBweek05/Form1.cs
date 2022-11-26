980d627f7d18a078c888c88b116a4ce176d62667
annamezo <annamezo@zohomail.eu>
2022-11-26 10:27:19 +0100

    Feladat06

--- MNBweek05/Form1.cs
+++ MNBweek05/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MNBweek05
@@ -25,6 +26,16 @@ namespace MNBweek05
             //GetRates();
 
             ReadXml();
+
+            chartRateData.DataSource = Rates;
+            chartRateData.Series[0].ChartType = SeriesChartType.Line;
+            chartRateData.Series[0].XValueMember = "date";
+            chartRateData.Series[0].YValueMembers = "value";
+            chartRateData.Series[0].BorderWidth = 2;
+            chartRateData.Legends[0].Enabled = false;
+            chartRateData.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chartRateData.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chartRateData.ChartAreas[0].AxisY.IsStartedFromZero = false;
         }
 
         private void ReadXml()
