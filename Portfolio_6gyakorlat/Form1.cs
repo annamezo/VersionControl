7040c92cbb7fb6b2d7c01d74c0dd608e2d0c3336
annamezo <annamezo@zohomail.eu>
2022-12-17 18:40:14 +0100

    feladat04

--- Portfolio_6gyakorlat/Form1.cs
+++ Portfolio_6gyakorlat/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace Portfolio_6gyakorlat
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
             InitializeComponent();
+
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
         }
     }
 }
